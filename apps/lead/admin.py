15089de7ff5af8c079749cfb5aee65e886a57fd5
Tejas Sawant <tejusawant302@gmail.com>
2023-10-19 01:46:41 +0530

    Update admin.py

--- apps/lead/admin.py
+++ apps/lead/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User,Student,Assignee
 
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Assignee)
 # Register your models here.
